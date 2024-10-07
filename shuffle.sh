85bf27c848de53fbd3524af20f3edfa6793573a4
yinshiyi <yinshiyi2020@gmail.com>
2024-10-07 13:59:19 -0700

    add shuffle path

--- shuffle.sh
+++ shuffle.sh
@@ -1,9 +1,12 @@
-time python3 ${SHUFFLE_SCRIPT_DIR}/shuffle_tfrecords_beam.py \
+YOUR_PROJECT=takara
+BASE="/home/syin/lol/data/training-case-study"
+OUTPUT_DIR="${BASE}/output2"
+time python3 tools/shuffle_tfrecords_beam.py \
   --project="${YOUR_PROJECT}" \
-  --input_pattern_list="${OUTPUT_DIR}"/validation_set.with_label.tfrecord-?????-of-00016.gz \
-  --output_pattern_prefix="${OUTPUT_DIR}/validation_set.with_label.shuffled" \
+  --input_pattern_list="${OUTPUT_DIR}"/training_set.with_label.tfrecord-?????-of-00007.gz \
+  --output_pattern_prefix="${OUTPUT_DIR}/training_set.with_label.shuffled" \
   --output_dataset_name="HG001" \
-  --output_dataset_config_pbtxt="${OUTPUT_DIR}/validation_set.dataset_config.pbtxt" \
+  --output_dataset_config_pbtxt="${OUTPUT_DIR}/training_set.dataset_config.pbtxt" \
   --job_name=shuffle-tfrecords \
   --runner=DirectRunner \
   --direct_num_workers=0
