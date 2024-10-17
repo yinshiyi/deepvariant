2c99126cf8d93432acfd66ec8204125e5e3084bd
yinshiyi <yinshiyi2020@gmail.com>
2024-10-16 17:36:24 -0700

    add shuffle validation

---
+++ shuffle_validation.sh
@@ -0,0 +1,12 @@
+YOUR_PROJECT=takara
+BASE="/home/ubuntu/data/training-case-study"
+OUTPUT_DIR="${BASE}/output"
+time python3 tools/shuffle_tfrecords_beam.py \
+  --project="${YOUR_PROJECT}" \
+  --input_pattern_list="${OUTPUT_DIR}"/validation_set.with_label.tfrecord-?????-of-?????.gz \
+  --output_pattern_prefix="${OUTPUT_DIR}/validation_set.with_label.shuffled" \
+  --output_dataset_name="HG001" \
+  --output_dataset_config_pbtxt="${OUTPUT_DIR}/validation_set.dataset_config.pbtxt" \
+  --job_name=shuffle-tfrecords \
+  --runner=DirectRunner \
+  --direct_num_workers=0
