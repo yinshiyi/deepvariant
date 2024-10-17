2d9f0cf56ad6b755a7bd7b60781d8d075497c2c9
Shiyi Yin <yinshiyi2020@gmail.com>
2024-10-17 19:04:24 +0000

    testing shuffle

--- shuffle_validation.sh
+++ shuffle_validation.sh
@@ -4,10 +4,10 @@ OUTPUT_DIR="${BASE}/output"
 time python3 tools/shuffle_tfrecords_beam.py \
   --project="${YOUR_PROJECT}" \
   --input_pattern_list="${OUTPUT_DIR}"/validation_set.with_label.tfrecord-?????-of-?????.gz \
-  --output_pattern_prefix="${OUTPUT_DIR}/validation_set.with_label.shuffled" \
+  --output_pattern_prefix="${OUTPUT_DIR}/2/validation_set.with_label.shuffled" \
   --output_dataset_name="HG001" \
-  --output_dataset_config_pbtxt="${OUTPUT_DIR}/validation_set.dataset_config.pbtxt" \
+  --output_dataset_config_pbtxt="${OUTPUT_DIR}/2/validation_set.dataset_config.pbtxt" \
   --job_name=shuffle-tfrecords \
   --runner=DirectRunner \
-  --direct_running_mode="multi_threading" \
-  --direct_num_workers=7
+  --direct_num_workers=0
+  # --direct_running_mode=multi_threading \
