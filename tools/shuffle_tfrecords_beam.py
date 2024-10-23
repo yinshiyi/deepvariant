5dc15991beac89ec6a16664018bdf99846873048
Shiyi Yin <38135561+yinshiyi@users.noreply.github.com>
2024-10-23 04:14:36 +0000

    update dateflow to spark

--- tools/shuffle_tfrecords_beam.py
+++ tools/shuffle_tfrecords_beam.py
@@ -1,70 +1,27 @@
-# Copyright 2018 Google LLC.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its
-#    contributors may be used to endorse or promote products derived from this
-#    software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
 # pylint: disable=line-too-long
-r"""Shuffle tf.Example files using beam.
+r"""Shuffle tf.Example files using Apache Beam with SparkRunner on AWS.
 
-To run locally:
-1) Install beam on your machine following the instructions at
-   https://beam.apache.org/get-started/quickstart-py/
+To run on AWS using the Spark Runner:
+1) Set up an AWS EMR cluster or use an existing Spark cluster.
 
-2) Copy any inputs to be on local disk.
+2) Upload the input files to an S3 bucket.
 
-3) Run
-  python path/to/shuffle_tfrecords_beam.py \
-    --input_pattern_list="/tmp/some.examples-?????-of-00200.tfrecord.gz" \
-    --output_pattern_prefix="/tmp/training.examples" \
+3) Run the following command on your Spark cluster or submit it through EMR:
+
+  python shuffle_tfrecords_beam_spark.py \
+    --input_pattern_list="s3://YOUR_INPUT_BUCKET/A.tfrecord.gz" \
+    --output_pattern_prefix="s3://YOUR_OUTPUT_BUCKET/training.examples" \
     --output_dataset_name="HG001" \
-    --runner=DirectRunner
+    --runner=SparkRunner \
+    --project=SET_YOUR_AWS_PROJECT_ID_HERE \
+    --region=SET_YOUR_AWS_REGION_HERE \
+    --staging_location=s3://YOUR_BUCKET_NAME/AND_STAGING_DIRECTORY \
+    --temp_location=s3://YOUR_BUCKET_NAME/AND_TEMP_DIRECTORY
 
-To run on Google Cloud Dataflow Service:
-1) Follow the Google Cloud Dataflow setup instructions at
-https://beam.apache.org/documentation/runners/dataflow/
+Make sure the AWS EMR cluster has access to read from and write to S3.
 
-2) Upload this file to your GCE instance.
+You can monitor the Spark job using the Spark UI.
 
-3) Run
-  python shuffle_tfrecords_beam.py \
-  --job_name=shuffle-tfrecords \
-  --input_pattern_list="gs://YOUR_INPUT_BUCKET/A.tfrecord.gz" \
-  --output_pattern_prefix="gs://YOUR_OUTPUT_BUCKET/training.examples" \
-  --output_dataset_name="HG001" \
-  --runner=DataflowRunner \
-  --project=SET_YOUR_PROJECT_ID_HERE \
-  --staging_location=gs://YOUR_BUCKET_NAME/AND_STAGING_DIRECTORY \
-  --temp_location=gs://YOUR_BUCKET_NAME/AND_TEMP_DIRECTORY
-
-4) (Optional) To monitor or cancel the job while it is running, you can
-use either the Dataflow Monitoring Interface
-https://cloud.google.com/dataflow/pipelines/dataflow-monitoring-intf
-or the Dataflow Command-line Interface
-https://cloud.google.com/dataflow/pipelines/dataflow-command-line-intf
 """
 # pylint: enable=line-too-long
 
@@ -91,9 +48,7 @@ def parse_cmdline(argv):
 
   The known arguments are required for this specific program to function,
   and the other pipeline arguments can be used to configure beam and the
-  specific beam backend being used.  See
-  https://github.com/apache/beam/blob/master/sdks/python/apache_beam/options/pipeline_options.py
-  for a list and description of the pipeline arguments accepted.
+  specific beam backend being used.
 
   Args:
     argv: List containing command-line arguments.
@@ -106,10 +61,10 @@ def parse_cmdline(argv):
 
   parser.add_argument(
       '--input_pattern_list',
-      help='Comma-separated list of TFRecord filename patterns.')
+      help='Comma-separated list of TFRecord filename patterns in S3.')
   parser.add_argument(
       '--output_pattern_prefix',
-      help='Filename pattern for the output TFRecords.')
+      help='Filename pattern for the output TFRecords in S3.')
   parser.add_argument(
       '--output_dataset_config_pbtxt',
       help='Optional.  If set, print out a human-readable version of '
@@ -128,7 +83,7 @@ def read_from_tfrecords_files(pipeline, input_filename_pattern_list):
 
   Args:
     pipeline: Beam pipeline object.
-    input_filename_pattern_list: List of filename patterns.
+    input_filename_pattern_list: List of filename patterns in S3.
 
   Returns:
     A PCollection of read tf.Examples.
@@ -142,7 +97,7 @@ def read_from_tfrecords_files(pipeline, input_filename_pattern_list):
 
 
 def shuffle_records(input_examples):
-  """Shuffles the input_examples in a effectively random order."""
+  """Shuffles the input_examples in an effectively random order."""
 
   def sha1(input_bytes):
     """Returns the sha1 hash of input_bytes."""
@@ -157,7 +112,7 @@ def shuffle_records(input_examples):
 
 
 def count_records_per_label(input_examples):
-  """Shuffles the input_examples in a effectively random order."""
+  """Counts records by label."""
 
   def label_example(input_bytes):
     """Returns the label of input_example."""
@@ -185,20 +140,8 @@ def make_config_string(name, tfrecord_path, num_examples):
 def write_summary_string_to_file(pipeline, output_examples, input_pattern_list,
                                  dataset_name, output_pattern_prefix,
                                  output_filename):
-  """Writes a file summarizing the PCollection of Examples.
+  """Writes a file summarizing the PCollection of Examples."""
 
-  Args:
-    pipeline: Beam pipeline object.
-    output_examples: PCollection of examples.
-    input_pattern_list: str. A comma-separated string of input files.
-    dataset_name: str. The name of the dataset to be written in the output.
-    output_pattern_prefix: str. The prefix of the sharded output files.
-    output_filename: the output text file that contains the summary that can be
-      parsed into DeepVariantDatasetConfig.
-  """
-
-  # Beam currently has no way to materialize pipeline values, so we have
-  # to construct the file entirely in Beam pipeline operations.
   comment_str = pipeline | 'CreateFileHeader' >> beam.Create(
       [COMMENT_HEADER.format(input_pattern_list, output_pattern_prefix)])
   num_examples = (
@@ -216,25 +159,18 @@ def write_summary_string_to_file(pipeline, output_examples, input_pattern_list,
       | 'WriteToFile' >> beam.io.WriteToText(
           output_filename,
           shard_name_template='',
-          header='# Generated by shuffle_tfrecords_beam.py'))
+          header='# Generated by shuffle_tfrecords_beam_spark.py'))
 
 
 def main(argv=None):
   """Main entry point; defines and runs the pipeline."""
   known_args, pipeline_args = parse_cmdline(argv)
-  # Copy over the example_info.json file before the pipeline starts.
-  example_info_json = '{}*example_info.json'.format(
-      known_args.input_pattern_list.split(',')[0])
-  example_info_json_list = tf.io.gfile.glob(example_info_json)
-  if example_info_json_list and tf.io.gfile.exists(example_info_json_list[0]):
-    training_dir = os.path.dirname(known_args.output_pattern_prefix)
-    if not tf.io.gfile.exists(training_dir):
-      tf.io.gfile.makedirs(training_dir)
-    output_example_info_json = os.path.join(training_dir, 'example_info.json')
-    if not tf.io.gfile.exists(output_example_info_json):
-      tf.io.gfile.copy(example_info_json_list[0], output_example_info_json)
 
   pipeline_options = PipelineOptions(pipeline_args)
+
+  # Ensure AWS-specific Spark configurations
+  pipeline_options.view_as(PipelineOptions).add_experiment('use_s3')
+
   with beam.Pipeline(options=pipeline_options) as p:
     input_examples = read_from_tfrecords_files(
         p, known_args.input_pattern_list.split(','))
